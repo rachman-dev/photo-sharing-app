45be71696cd826d08fa04907a25455a9fa77d2b7
Rachman Sayed <rachman-dev@outlook.com>
2024-07-25 09:32:02 +0300

    env

--- frontend/src/services/PhotoServices.js
+++ frontend/src/services/PhotoServices.js
@@ -2,27 +2,27 @@ import axios from 'axios';
 
 
 export const getAllPhotos = async () => {
-    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/photos`);
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/photos`);
     return data;
 }
 
 export const getPhotosByUserId = async (userId) => {
-    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/photos/publisher/${userId}`);
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/photos/publisher/${userId}`);
     return data;
 };
 
 export const getPhotosByCategoryId = async (categoryId) => {
-    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/photos/category/${categoryId}`);
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/photos/category/${categoryId}`);
     return data;
 }
 
 export const getPhotoById = async (photoId) => {
-    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/photos/${photoId}`);
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/photos/${photoId}`);
     return data;
 };
 
 export const addPhoto = async (imageUrl, title, description, category, publisher, publisherName) => {
-    const { data } = await axios.post(`${process.env.REACT_APP_BASE_URL}/photos/`, {
+    const { data } = await axios.post(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/photos/`, {
         imageUrl,
         title,
         description,
@@ -36,11 +36,11 @@ export const addPhoto = async (imageUrl, title, description, category, publisher
 export const uploadPhotoToCloudinary = async (image) => {
     const data = new FormData();
     data.append("file", image);
-    data.append("upload_preset", `${process.env.REACT_APP_UPLOAD_PRESET}`);
-    data.append("cloud_name", `${process.env.REACT_APP_CLOUD_NAME}`);
+    data.append("upload_preset", `${process.env.REACT_APP_UPLOAD_PRESET || "photo_sharing_app"}`);
+    data.append("cloud_name", `${process.env.REACT_APP_CLOUD_NAME || "dw4jafagr"}`);
 
     try {
-        const response = await fetch(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/image/upload`, {
+        const response = await fetch(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME || "dw4jafagr"}/image/upload`, {
             method: 'POST',
             body: data
         });
@@ -66,7 +66,7 @@ export const uploadPhotoToCloudinary = async (image) => {
 
 
 export const updatePhoto = async (photoId, title, description, category) => {
-    const { data } = await axios.put(`${process.env.REACT_APP_BASE_URL}/photos/${photoId}`, {
+    const { data } = await axios.put(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/photos/${photoId}`, {
         title,
         description,
         category
@@ -75,7 +75,7 @@ export const updatePhoto = async (photoId, title, description, category) => {
 };
 
 export const deletePhoto = async (photoId) => {
-    const { data } = await axios.delete(`${process.env.REACT_APP_BASE_URL}/photos/${photoId}`);
+    const { data } = await axios.delete(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/photos/${photoId}`);
     return data;
 }
 
@@ -83,14 +83,14 @@ export const deletePhoto = async (photoId) => {
 
 export const addLike = (photoId,likerId) => {
 
-    axios.put(`${process.env.REACT_APP_BASE_URL}/photos/${photoId}/likes/${likerId}`,{
+    axios.put(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/photos/${photoId}/likes/${likerId}`,{
         liker:likerId
     });
 };
 
 export const removeLike = (photoId,likerId) => {
     const id=likerId
-    axios.delete(`${process.env.REACT_APP_BASE_URL}/photos/${photoId}/likes/${likerId}`,{
+    axios.delete(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/photos/${photoId}/likes/${likerId}`,{
         liker:id
     });
 }
--- frontend/src/services/UserServices.js
+++ frontend/src/services/UserServices.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
 export const getUser = async (userId) => {
-    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/users/${userId}`);
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/users/${userId}`);
     return data;
 };
 
 export const followUser = (followerId, followedId) => {
-    axios.post(`${process.env.REACT_APP_BASE_URL}/users/${followedId}/followers/${followerId}`);
+    axios.post(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/users/${followedId}/followers/${followerId}`);
 };
 
 export const unfollowUser = (followerId, followedId) => {
-    axios.delete(`${process.env.REACT_APP_BASE_URL}/users/${followedId}/followers/${followerId}`);
+    axios.delete(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/users/${followedId}/followers/${followerId}`);
 };
 
 export const setProfilePhoto = async (userId, image) => {
-    const { data } = await axios.put(`${process.env.REACT_APP_BASE_URL}/users/${userId}/photo`, {
+    const { data } = await axios.put(`${process.env.REACT_APP_BASE_URL || "https://photo-sharing-app-rho.vercel.app"}/users/${userId}/photo`, {
         imageUrl: image
     });
     return data;
